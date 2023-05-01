cbf9a4a7ebae074ce864a962a1bacfe5919eabf7
LisandroDiMeo <ddmlisandro22@gmail.com>
2023-05-01 01:49:34 -0300

    Paralellize smali mutants

--- extra/smali_mutants.py
+++ extra/smali_mutants.py
@@ -1,8 +1,8 @@
-import subprocess
 import argparse
-import json
 import glob
 import os
+import subprocess
+import concurrent.futures
 
 # Create arg parser for Smaling Mutants
 parser = argparse.ArgumentParser(description="Argument parser for Smaling Mutants")
@@ -38,46 +38,53 @@ subprocess.run([
     "-f"])
 print("> Baksmali finished!")
 
-for idx, path in enumerate(paths):
-    mutated_file = os.listdir(f"./{args.mutants_path}/{path}")[0]
-    mutated_file_path = f"./{args.mutants_path}/{path}/{mutated_file}"
-    mutant_dir = args.output_dir + "/mutant-" + str(idx)
+
+def process_mutant(mutant_id, file_path, program_args, baksmali_apk_path):
+    mutated_file = os.listdir(f"./{program_args.mutants_path}/{file_path}")[0]
+    mutated_file_path = f"./{program_args.mutants_path}/{file_path}/{mutated_file}"
+    mutant_dir = program_args.output_dir + "/mutant-" + str(mutant_id)
     subprocess.run(["mkdir", mutant_dir])
     subprocess.run(["cp", "-R", baksmali_apk_path, mutant_dir])
     # find the file to change TODO: This only takes AST Changes, strings, colors and Manifest will be added
-    directories_to_search = glob.glob(mutant_dir+'/temp/smali*')
+    directories_to_search = glob.glob(mutant_dir + '/temp/smali*')
     file_to_change_path = ""
     for directory in directories_to_search:
-        file_pattern = "*"+mutated_file
+        file_pattern = "*" + mutated_file
         find_command = ["find", directory, "-type", "f", "-name", file_pattern]
         result = subprocess.run(find_command, stdout=subprocess.PIPE)
         output = result.stdout.decode('utf-8')
         if len(output) > 0:
             file_to_change_path = output
             break
-    print("The file to change is at " + file_to_change_path)
+    print(f"The file to change for mutant {mutant_id} is at " + file_to_change_path)
     dir_of_file_to_change = os.path.dirname(file_to_change_path)
     # override the file and smali files
     subprocess.run(["cp", mutated_file_path, dir_of_file_to_change])
-    print("> Smalling the mutated apk...")
+    print(f"> Smalling the mutant {mutant_id} apk...")
     subprocess.run([
         "java",
         "-jar",
-        f"{args.apk_tool_path}/apktool.jar",
+        f"{program_args.apk_tool_path}/apktool.jar",
         "b",
-        mutant_dir+"/temp",
+        mutant_dir + "/temp",
         "-o",
-        mutant_dir+"/"+os.path.basename(args.apk_path),
+        mutant_dir + "/" + os.path.basename(program_args.apk_path),
         "-f"])
-    print("> Smalling finished. Now signing it...")
+    print(f"> Smalling finished for mutant {mutant_id}. Now signing it...")
     subprocess.run([
         "java",
         "-jar",
-        f"{args.apk_tool_path}/uber-apk-signer.jar",
+        f"{program_args.apk_tool_path}/uber-apk-signer.jar",
         "-a",
-        mutant_dir+"/"+os.path.basename(args.apk_path)
+        mutant_dir + "/" + os.path.basename(program_args.apk_path)
     ])
-    print("> Signing process finished... ")
-    if args.delete_non_signed == "y":
-        print("> Removing non signed apk...")
-        subprocess.run(["rm", mutant_dir+"/"+os.path.basename(args.apk_path)])
+    print(f"> Signing process for mutant {mutant_id} finished... ")
+    if program_args.delete_non_signed == "y":
+        print(f"> Removing non signed for mutant {mutant_id} apk...")
+        subprocess.run(["rm", mutant_dir + "/" + os.path.basename(program_args.apk_path)])
+
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+    for idx, path in enumerate(paths):
+        mutation_process = executor.submit(process_mutant, idx, path, args, baksmali_apk_path)
+
